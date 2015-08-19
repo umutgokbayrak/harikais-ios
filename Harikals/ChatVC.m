8c50e46e4d024c09dfb90a0715174e3d26b727af
Umut Gokbayrak <umutgokbayrak@yahoo.com>
2015-08-19 13:33:38 +0300

    fixed the bug for "crashes when a chat is clicked when selected from the left menu"

--- Harikals/ChatVC.m
+++ Harikals/ChatVC.m
@@ -306,9 +306,12 @@
     ChatCell *cell = [tableView dequeueReusableCellWithIdentifier:direction % 2 != 0 ? @"myChatCell" : @"otherChatCell"];
     cell.messageTextView.text = messagesArray[indexPath.row][@"msg"];
     cell.avatarImageView.layer.cornerRadius = cell.avatarImageView.frame.size.width / 2.0;
-
     
-    NSString *fromString = direction % 2 != 0 ? @"Siz" : dataDictionary[@"company"][@"name"];
+    NSObject *companyObj = dataDictionary[@"company"];
+
+    NSString *companyName = (companyObj != nil && [companyObj isKindOfClass:[NSString class]]) ? dataDictionary[@"company"] : dataDictionary[@"company"][@"name"];
+    
+    NSString *fromString = direction % 2 != 0 ? @"Siz" : companyName;
     if ([fromString isEqualToString:@"You"]) {
         fromString = @"Siz";
     }
